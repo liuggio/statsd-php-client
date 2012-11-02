228cbd33a572c5e53421123b7c0b8bab5c732bd0
liuggio <liuggio@gmail.com>
2012-11-02 18:47:56 +0100

    dump sender

---
+++ src/Liuggio/StatsdClient/Sender/EchoSender.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Liuggio\StatsdClient\Sender;
+
+
+Class EchoSender implements SenderInterface
+{
+    /**
+     * @inherit
+     */
+    public function open($hostname, $port = null, $protocol = null) {
+        echo "[open]";
+        return true;
+    }
+
+    /**
+     * @inherit
+     */
+    function write($handle, $message, $length = null){
+        echo "[$message]";
+        return strlen($message);
+    }
+
+    /**
+     * @inherit
+     */
+    function close($handle){
+        echo "[closed]";
+    }
+}
